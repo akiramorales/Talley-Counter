15d82470a024347f609d5a65c381608ea3147936
Akira Morales <akira.k.morales@gmail.com>
2018-09-26 12:51:25 -0400

    Add files via upload

---
+++ TalleyCounterExample/src/TalleyCounter.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ * @author Akira Morales
+ *
+ */
+public class TalleyCounter
+{
+
+	//fields
+	private int count;
+	
+	
+	//constructors
+	public TalleyCounter()
+	{
+		count = 0;
+	}
+	
+	public TalleyCounter(int a)
+	{
+		count = a;
+	}
+	
+	//method
+	public void click()
+	{
+		count++; // count = count + 1;
+				 // count += 1
+	}
+	
+	
+	public int getCount()
+	{
+		return count;
+	}
+	
+	
+	public void reset()
+	{
+		count = 0;
+	}
+	
+	
+	public void unclick()
+	{
+		count--;
+	}
+}
